f8cf755b90e54b58f53af92c1fe69488720cec3f
Archwizard <archwizard101@gmail.com>
2019-06-17 11:04:09 -0700

    Added priority to Non-Linux logging

--- dualLoggerCross.go
+++ dualLoggerCross.go
@@ -4,24 +4,30 @@ package main
 
 import (
 	"log"
+    "fmt"
 )
 
 func SetupLogging () {
-    log.Print("Note: Non-Linux systems don't support this implimentation of syslog; writing messages to Standard Output instead")
+    DualNotice("Non-Linux systems don't support this implimentation of syslog; writing messages to Standard Output instead")
 }
 
 func DualDebug (m string) {
+    m = fmt.Sprintf("DEBUG: %s", m)
     log.Print(m)
 }
 func DualInfo (m string) {
+    m = fmt.Sprintf("INFO: %s", m)
     log.Print(m)
 }
 func DualNotice (m string) {
+    m = fmt.Sprintf("NOTICE: %s", m)
     log.Print(m)
 }
 func DualWarning (m string) {
+    m = fmt.Sprintf("WARNING: %s", m)
     log.Print(m)
 }
 func DualErr (err error) {
-    log.Fatal(err)
+    m := fmt.Sprintf("FATAL: %s", err.Error())
+    log.Fatal(m)
 }
