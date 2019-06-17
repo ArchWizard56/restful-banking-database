2a89bda81cbf1f310ba32a5549c0875ce8fe2dcb
Archwizard <archwizard101@gmail.com>
2019-06-17 10:02:20 -0700

    Added syslog support

--- dualLoggerLinux.go
+++ dualLoggerLinux.go
@@ -3,28 +3,40 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"log"
-    "time"
-    "runtime"
+    "log/syslog"
+    "os"
+    //"time"
 )
 
+var SystemSyslog *syslog.Writer
+
 func SetupLogging () {
-    log.Print("Note: Non-Linux systems don't support this implimentation of syslog; writing messages to Standard Output instead")
+    var err error
+    SystemSyslog, err = syslog.New(syslog.LOG_INFO,os.Args[0])
+    if err != nil {
+    log.Fatal(err)
+    }
 }
 
 func DualDebug (m string) {
+    SystemSyslog.Debug(m)
     log.Print(m)
 }
 func DualInfo (m string) {
+    SystemSyslog.Write([]byte(m))
     log.Print(m)
 }
 func DualNotice (m string) {
+    SystemSyslog.Notice(m)
     log.Print(m)
 }
 func DualWarning (m string) {
+    SystemSyslog.Warning(m)
     log.Print(m)
 }
 func DualErr (err error) {
+    SystemSyslog.Err(err.Error())
     log.Fatal(err)
 }
