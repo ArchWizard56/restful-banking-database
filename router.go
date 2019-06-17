c6ff9fa4f1eab55e7f8a64e9ef627c506bcfd613
Archwizard <archwizard101@gmail.com>
2019-06-17 12:19:48 -0700

    Added debug output

--- router.go
+++ router.go
@@ -1,45 +1,46 @@
 package main
+
 import (
-    "github.com/gorilla/mux"
-    "net/http"
-    "fmt"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
 )
+
 type Route struct {
-    Name string
-    Method string
-    Path string
-    Handler http.HandlerFunc
+	Name    string
+	Method  string
+	Path    string
+	Handler http.HandlerFunc
 }
 
 //Handler functions
-func Placeholder (w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Placeholder"))
-    DualInfo(fmt.Sprintf("%s request from %s to %s", r.Method,r.RemoteAddr,r.URL.Path))
+func Placeholder(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Placeholder"))
+	DualDebug(fmt.Sprintf("%s request from %s to %s", r.Method, r.RemoteAddr, r.URL.Path))
 }
 
 //Use Route structs to construct all the necessary routes
-func InitRouter () *mux.Router {
-// List of all routes for API
-TheRoutes := []Route{
-    Route {
-        "Index",
-        "Get",
-        "/",
-        Placeholder,
-    },
-    Route {
-        "Register",
-        "Post",
-        "/register",
-        Placeholder,
-    },
+func InitRouter() *mux.Router {
+	// List of all routes for API
+	TheRoutes := []Route{
+		Route{
+			"Index",
+			"Get",
+			"/",
+			Placeholder,
+		},
+		Route{
+			"Register",
+			"Post",
+			"/register",
+			Placeholder,
+		},
+	}
+	router := mux.NewRouter()
+	DualInfo("Loading Routes")
+	//Loop over the list of Routes and add them to the router
+	for _, r := range TheRoutes {
+		router.HandleFunc(r.Path, r.Handler).Methods(r.Method)
+	}
+	return router
 }
-router := mux.NewRouter()
-DualInfo("Loading Routes")
-//Loop over the list of Routes and add them to the router
-for _, r := range TheRoutes {
-    router.HandleFunc(r.Path, r.Handler).Methods(r.Method)
-}
-return router
-}
-
