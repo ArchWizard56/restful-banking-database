6daba284026c68d7a8cc1a7f0500123b4712182a
Archwizard <archwizard101@gmail.com>
2019-06-17 09:40:54 -0700

    Implimented wrapper functions for logging without syslog, for cross platform use

---
+++ dualLoggerCross.go
@@ -0,0 +1,28 @@
+// +build !linux
+
+package main
+
+import (
+	"log"
+    "time"
+)
+
+func SetupLogging () {
+    log.Print("Note: Non-Linux systems don't support this implimentation of syslog; writing messages to Standard Output instead")
+}
+
+func DualDebug (m string) {
+    log.Print(m)
+}
+func DualInfo (m string) {
+    log.Print(m)
+}
+func DualNotice (m string) {
+    log.Print(m)
+}
+func DualWarning (m string) {
+    log.Print(m)
+}
+func DualErr (err error) {
+    log.Fatal(err)
+}
