7cddc971651f202ad6aea900a48bd32782b6e026
Archwizard <archwizard101@gmail.com>
2019-06-23 09:07:40 -0700

    Now respects debug flag

--- dualLoggerCross.go
+++ dualLoggerCross.go
@@ -6,14 +6,19 @@ import (
 	"log"
     "fmt"
 )
-
-func SetupLogging () {
+var Debug bool
+func SetupLogging (debug bool) {
     DualNotice("Non-Linux systems don't support this implimentation of syslog; writing messages to Standard Output instead")
+	if debug {
+		Debug = true
+	}
 }
 
 func DualDebug (m string) {
+    if Debug {
     m = fmt.Sprintf("DEBUG: %s", m)
     log.Print(m)
+}
 }
 func DualInfo (m string) {
     m = fmt.Sprintf("INFO: %s", m)
